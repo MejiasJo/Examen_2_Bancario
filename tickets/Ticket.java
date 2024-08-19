cc1cd6eb1be9eeeac066ab4d47bd6d555d604743
Johelmjs <jomejiasma@est.utn.ac.cr>
2024-08-19 14:50:37 -0600

    Se creo la clase Ticket
    La clase ticket tiene sus metodos, atributos y constructor
    
    EstadoTicket es un enum para el atributo de estado en Ticket

--- tickets/Ticket.java
+++ tickets/Ticket.java
@@ -9,5 +9,29 @@ package tickets;
  * @author Student
  */
 public class Ticket {
+    private int numero;
+    private EstadoTicket tipo;
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public EstadoTicket getTipo() {
+        return tipo;
+    }
+
+    public Ticket(int numero, EstadoTicket tipo) {
+        this.numero = numero;
+        this.tipo = tipo;
+    }
+    
+    public Ticket(){
+        this(0,null);
+    }
+
+    @Override
+    public String toString() {
+        return "Ticket{" + "numero=" + numero + ", tipo=" + tipo + '}';
+    }
     
 }
